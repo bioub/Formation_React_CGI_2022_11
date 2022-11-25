fc5ee517de5e297d5fa4b6a2c1dd1b7f2cf87d15
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-11-25 16:38:28 +0100

    Dernier Exercice

--- my-app/src/user/UserDetails.js
+++ my-app/src/user/UserDetails.js
@@ -1,4 +1,16 @@
+import { useParams } from "react-router-dom";
+
 function UserDetails() {
+  const { userId } = useParams();
+
+  /* Exercice
+  En vous inspirant de User
+  Créer 2 actions userFetchById et userFetchByIdSuccess
+  qui mettront à jour la clé user.currentUser du state avec les données du serveur
+  Terminer la fonction fetchUserById dans api.js
+  et l'appeler ainsi que le dispatch des actions dans un useEffect
+  */
+
   return (
     <div className="UserDetails">
       UserDetails Component
